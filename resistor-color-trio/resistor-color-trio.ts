c96e8ef5553fe5870a6f19e559c4deecda049695
Amelia Lim <limchornguan418@gmail.com>
2023-06-25 16:03:46 +0200

    improve trio

--- resistor-color-trio/resistor-color-trio.ts
+++ resistor-color-trio/resistor-color-trio.ts
@@ -11,21 +11,19 @@ const resistorBandValue = {
   white: 9,
 } as const;
 type ColourCode = keyof typeof resistorBandValue;
-const units = ["ohms", "kiloohms", "megaohms", "gigaohms"] as const;
+
+const units = [
+  [1 * 10 ** 9, "giga"],
+  [1 * 10 ** 6, "mega"],
+  [1 * 10 ** 3, "kilo"],
+] as const;
 
 export function decodedResistorValue([colour1, colour2, factor]: ColourCode[]) {
   const value =
     (resistorBandValue[colour1] * 10 + resistorBandValue[colour2]) *
     10 ** resistorBandValue[factor];
 
-  const zeroCount: number = value
-    .toString()
-    .split("")
-    .reduce((count: number, curr: string): number => {
-      if (curr === "0") return count + 1;
-      return count + curr === "0" ? 1 : 0;
-    }, 0);
-  const unitIndex = Math.floor(zeroCount / 3);
+  const [divisor, unit] = units.find(([unitValue]) => value >= unitValue) ?? [1, ""]
 
-  return `${value / 10 ** (unitIndex * 3)} ${units[unitIndex]}`;
+  return `${value / divisor} ${unit}ohms`;
 }
